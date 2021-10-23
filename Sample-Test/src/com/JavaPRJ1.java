021fa221fa11053b6249ca614923feaf75946dff
Prietthi <92286803+Prietthi@users.noreply.github.com>
2021-10-23 10:21:52 +0100

    Sample Test
    Test Sample push

---
+++ Sample-Test/src/com/JavaPRJ1.java
@@ -0,0 +1,12 @@
+package com;
+
+public class JavaPRJ1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hi here is String program");
+		System.out.println("Hi here is String program");		
+
+	}
+
+}
