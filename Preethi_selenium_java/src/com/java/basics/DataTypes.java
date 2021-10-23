b8c6d37e4e287e7be79302f8d008179dc47a1310
Prietthi <92286803+Prietthi@users.noreply.github.com>
2021-10-23 09:56:42 +0100

    First Commit
    Added a First Java class

---
+++ Preethi_selenium_java/src/com/java/basics/DataTypes.java
@@ -0,0 +1,20 @@
+package com.java.basics;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+
+		byte b=112;
+		short s=2145;
+		int i=45785;
+		long l = 365895;
+		char c='t';
+		float f=13.6f;
+		double d=436.756;
+		boolean bflag=true;
+		
+		System.out.println("Byte Value of B is : " +b);
+		System.out.println("Long value is : " +l);
+	}
+
+}
